4781df9c8cd2a98bff71ad7628a7cb313d586367
emanuel71jo <emanuel71jo@gmail.com>
2020-08-18 10:22:37 -0300

    Add codigos IA e TEBD

---
+++ codigos-IA/buscagenerica.py
@@ -0,0 +1,108 @@
+from __future__ import annotations
+from typing import Any, Sequence, Generic, Optional, Callable, TypeVar, Iterable
+from typing_extensions import Protocol
+
+'''
+Classes que resolvem buscas: dfs, bfs e ucs
+'''
+
+# Trabalhando com type hints (PEP 484)
+
+T = TypeVar('T')
+C = TypeVar('C', bound='Comparable')
+
+def busca_linear(iterador: Iterable[T], chave: T) -> bool:
+    for item in iterador:
+        if item == chave:
+            return True
+    return False
+
+class Comparable(Protocol):
+    def __eq__(self, other: Any) -> bool:
+        ...
+    
+    def __lt__(self: C, other: C) -> bool:
+        ...
+    
+    def __gt__(self: C, other: C) -> bool:
+        return (not self < other) and self != other
+    
+    def __le__(self: C, other: C) -> bool:
+        return self < other or self == other
+    
+    def __ge__(self: C, other: C) -> bool:
+        return not self < other
+
+def busca_binaria(sequencia: Sequence[C], chave: C) -> bool:
+    inicio: int = 0
+    fim: int = len(sequencia) - 1
+    
+    while inicio <= fim:
+        meio: int = (inicio + fim) // 2
+        if(sequencia[meio] < chave):
+            inicio = meio + 1
+        elif sequencia[meio] > chave:
+            fim = meio - 1
+        else:
+            return True
+    return False
+
+class Pilha(Generic[T]):
+    def __init__(self) -> None:
+        self._container: List[T] = []
+    
+    @property
+    def vazia(self) -> bool:
+        return not self._container
+    
+    def push(self, item: T) -> None:
+        self._container.append(item)
+        
+    def pop(self) -> T:
+        return self._container.pop()
+    
+    def __repr__(self) -> str:
+        return repr(self._container)
+
+class Noh(Generic[T]):
+    def __init__(self, estado: T, pai: Optional[Noh], custo: float = 0.0, heuristica: float = 0.0) -> None:
+        self.estado: T = estado
+        self.pai: Optional[Noh] = pai
+        self.custo: float = custo
+        self.heuristica: float = heuristica
+        
+    def __lt__(self, other: Noh) -> bool:
+        return (self.custo + self.heuristica) < (other.custo + other.heuristica)
+
+def profundidade(inicial: T, teste_objetivo: Callable[[T], bool], sucessores: Callable[[T], List[T]]) -> Optional[Noh[T]]:
+    fronteira: Pilha[Noh[T]] = Pilha()
+    fronteira.push(Noh(inicial, None))
+    
+    explorados: Set[T] = { inicial }
+        
+    while not fronteira.vazia:
+        noh_atual: Noh[T] = fronteira.pop()
+        estado_atual: Noh[T] = noh_atual.estado
+        
+        if teste_objetivo(estado_atual):
+            return noh_atual
+        
+        for filho in sucessores(estado_atual):
+            if filho in explorados:
+                continue
+            
+            explorados.add(filho)
+            fronteira.push(Noh(filho, noh_atual))
+    return None
+
+def percurso(noh_: Noh[T]) -> List[T]:
+    caminho: List[T] = [noh_.estado]
+    while noh_.pai is not None:
+        noh_ = noh_.pai
+        caminho.append(noh_.estado)
+    caminho.reverse()
+    return caminho
+
+if __name__ == '__main__':
+    print(busca_binaria([1,2,3,4,5,6,7,8,9,10], -1))
+    print(busca_binaria(['s', 'ss'], 's'))
---
+++ codigos-IA/labirinto.py
@@ -0,0 +1,75 @@
+'''Classes que simulam o ambiente'''
+
+from buscagenerica import profundidade, percurso, Noh
+from typing import List, Callable, Optional, NamedTuple
+from enum import Enum
+import random
+
+class Celula(str, Enum):
+  INICIAL = 'S'
+  OBJETIVO = 'G'
+  CAMINHO = '*'
+  VAZIA = ' '
+  BLOQUEADA = 'X'
+
+class Coordenadas(NamedTuple):
+  linha: int
+  coluna: int
+
+class Labirinto:
+  def __init__(self, linhas: int = 10, colunas: int = 10, disperso: float = 0.20, inicial: Coordenadas = Coordenadas(0,0), objetivo: Coordenadas = Coordenadas(9,9)) -> None:
+    self._linhas: int = linhas
+    self._colunas: int = colunas
+    self.inicial: Coordenadas = inicial
+    self.objetivo: Coordenadas = objetivo
+
+    self._grade = [[Celula.VAZIA for c in range(colunas)] for l in range(linhas)]
+    self.preencher_aleatoriamente(linhas, colunas, disperso)
+    self._grade[inicial.linha][inicial.coluna] = Celula.INICIAL 
+    self._grade[objetivo.linha][objetivo.coluna] = Celula.OBJETIVO
+
+  def preencher_aleatoriamente(self, linhas: int, colunas: int, disperso: float):
+    for linha in range(linhas):
+      for coluna in range(colunas):
+        if random.uniform(0, 1.0) < disperso:
+          self._grade[linha][coluna] = Celula.BLOQUEADA
+
+  def teste_objetivo(self, posicao: Coordenadas) -> bool:
+    return posicao == self.objetivo
+  
+  def sucessores(self, posicao: Coordenadas) -> List[Coordenadas]:
+    posicoes: List[Coordenadas] = []
+
+    if posicao.linha + 1 < self._linhas and self._grade[posicao.linha + 1][posicao.coluna] != Celula.BLOQUEADA:
+      posicoes.append(Coordenadas(posicao.linha + 1, posicao.coluna))
+    
+    if posicao.linha - 1  >= 0 and self._grade[posicao.linha - 1][posicao.coluna] != Celula.BLOQUEADA:
+      posicoes.append(Coordenadas(posicao.linha - 1, posicao.coluna))
+    
+    if posicao.coluna + 1 < self._colunas and self._grade[posicao.linha][posicao.coluna + 1] != Celula.BLOQUEADA:
+      posicoes.append(Coordenadas(posicao.linha, posicao.coluna + 1))
+
+    if posicao.coluna -1 >= self._colunas and self._grade[posicao.linha][posicao.coluna - 1] != Celula.BLOQUEADA:
+      posicoes.append(Coordenadas(posicao.linha, posicao.coluna - 1))
+
+    return posicoes
+  
+  def __str__(self) -> str:
+    saida: str = ''
+    for i in self._grade:
+      saida += ''.join([c.value for c in i]) + '\n'
+
+    return saida
+
+if __name__ == '__main__':
+  testeLabirinto = Labirinto()
+  print(testeLabirinto)
+
+  buscaProfundidade = profundidade(testeLabirinto.inicial, testeLabirinto.teste_objetivo, testeLabirinto.sucessores)
+
+  if buscaProfundidade is None:
+    print('------------')
+    print('Sem solução')
+  else:
+    print('------------')
+    percurso(buscaProfundidade)
